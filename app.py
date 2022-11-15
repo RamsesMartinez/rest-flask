58415c0a1c8f9619b782366f1b7cc81425c62e02
Ramses Martinez <ramses.mtz96@gmail.com>
2022-11-14 20:54:17 -0600

    nuevos cambios

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask_restful import Api
+from db import database
 
+from routes.users_routes import users_blueprint
 
 app = Flask(__name__)
 api = Api(app)
 
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+app.register_blueprint(users_blueprint)
+
+
 if __name__ == "__main__":
+    database.create_database("./db/database.db")
     app.run()
---
+++ db/database.py
@@ -0,0 +1,8 @@
+# SQLITE
+import sqlite3
+
+
+def create_database(path: str):
+    """Función encargada de generar la base de datos inicial."""
+    # print(path)
+    pass
